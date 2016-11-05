cd37adf6effa1b442e9088c58103f429e0300273
kevinshurtzacu <kms14d@acu.edu>
2016-11-05 15:49:19 -0500

    consolidated informational files, jdbc connection works, data module begun

---
+++ data/config_schedule_db.sql
@@ -0,0 +1,45 @@
+DROP TABLE IF EXISTS Student;
+CREATE TABLE Student(
+    banner_id   INTEGER,
+    first_name  VARCHAR(30),
+    last_name   VARCHAR(30),
+    class       VARCHAR(5),
+    PRIMARY KEY(banner_id));
+
+CREATE TABLE Professor(
+    professor_id    INTEGER,
+    first_name      VARCHAR(30),
+    last_name       VARCHAR(30),
+    PRIMARY KEY(professor_id));
+
+CREATE TABLE Section(
+    crn             INTEGER,
+    term            VARCHAR(6),
+    term_length     VARCHAR(30),
+    class_subject   VARCHAR(10),
+    class_number    VARCHAR(3),
+    class_title     VARCHAR(70),
+    class_suffix    VARCHAR(5),
+    year            INTEGER,
+    time_start      INTEGER,
+    time_end        INTEGER,
+    PRIMARY KEY(crn));
+
+CREATE TABLE Enroll(
+    banner_id   INTEGER,
+    crn         INTEGER,
+    PRIMARY KEY(banner_id, crn),
+    CONSTRAINT fk_enroll_has_banner FOREIGN KEY(banner_id)
+        REFERENCES Student(banner_id),
+    CONSTRAINT fk_enroll_has_crn FOREIGN KEY(crn)
+        REFERENCES Section(crn));
+
+CREATE TABLE Assign(
+    professor_id    INTEGER,
+    crn             INTEGER,
+    PRIMARY KEY(professor_id, crn),
+    CONSTRAINT fk_assign_has_prof_id FOREIGN KEY(professor_id)
+        REFERENCES Professor(professor_id),
+    CONSTRAINT fk_assign_has_crn FOREIGN KEY(crn)
+        REFERENCES Section(crn));
+
--- src/test/java/scheduler/DataModuleSteps.java
+++ src/test/java/scheduler/DataModuleSteps.java
@@ -7,21 +7,123 @@ import cucumber.api.java.en.When;
 import static org.junit.Assert.*;
 import org.apache.commons.io.IOUtils;
 
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 class DataModule
 {
-    public void load(String path)
+    private static final String CONNECTION_URL =
+            "jdbc:sqlite:data/schedule_data.db";
+
+    private static Connection conn = null;
+    private static Statement stmt = null;
+    private static ResultSet rs = null;
+
+    public static void load(String csvPath) throws SQLException
+    {
+        try {
+            conn = DriverManager.getConnection(CONNECTION_URL);
+            stmt = conn.createStatement(ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_UPDATABLE);
+            /*
+            Scanner scan = new Scanner(new File(csvPath));
+
+            while (scan.hasNextLine()) {
+
+            }
+            */
+        }
+        catch (SQLException e) {
+            System.err.println(e);
+        }
+        finally {
+            if (rs != null)
+                rs.close();
+
+            if (stmt != null)
+                stmt.close();
+
+            if (conn != null)
+                conn.close();
+        }
+    }
+
+    public void insertStudent(Student student)
     {
 
+    }
+
+    public void insertProfessor(Professor prof)
+    {
+
+    }
+
+    public void insertSection(Section section)
+    {
+
+    }
+
+    public Student makeStudent(String firstName, String lastName, String classification)
+    {
+        if (firstName == null || lastName == null || classification == null)
+            return null;
+
+        return new Student(firstName, lastName, classification);
+    }
+
+    public Professor makeProfessor(String firstName, String lastName)
+    {
+        if (firstName == null || lastName == null)
+            return null;
+
+        return new Professor(firstName, lastName);
+    }
+
+    public Section makeSection(Integer crn, String cTerm, String cTermLength, String sub,
+                            String num, String tit, String suf, Integer year, Integer start, Integer end)
+    {
+        Term sectionTerm = null;
+        TermLength sectionTermLength = null;
+
+        for (Term term : Term.values())
+        {
+            if (cTerm.equals(term.name()))
+                sectionTerm = term;
+        }
+
+        for (TermLength termLength : TermLength.values())
+        {
+            if (cTermLength.equals(termLength.name()))
+                sectionTermLength = termLength;
+        }
+
+        if (crn == null
+                || sectionTerm == null
+                || sectionTermLength == null
+                || sub == null
+                || num == null
+                || tit == null
+                || suf == null
+                || year == null
+                || start == null
+                || end == null)
+            return null;
+
+        return new Section(crn, sectionTerm, sectionTermLength, sub, num, tit, suf, year, start, end);
     }
 }
 
 public class DataModuleSteps
 {
+    static String fileName;
+
     @Given("^the file \"([^\"]*)\" to load$")
-    public void theFileToLoad(String fileName) throws Throwable
+    public void theFileToLoad(String file) throws Throwable
     {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+        fileName = file;
+//        DataModule.load();
     }
 
     @When("^I load the file into the data module$")
